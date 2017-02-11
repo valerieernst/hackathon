063db5e9f68a8d90a8f01a633db24d0c5a573e36
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-02-11 13:08:42 -0800

    Create basic webpack set up {VE}

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const webpack = require('webpack');
+const webpackConfig = require('../webpackConfig.js');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const port = process.env.PORT || 8008;
 
 const app = express();
-const port = process.env.PORT || 8008;
+
+const compiler = webpack(webpackConfig);
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: "/",
+  stats: {
+    // Config for minimal console.log mess.
+    assets: false,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false
+  }
+}));
 
 app.use(bodyParser.json());
 
