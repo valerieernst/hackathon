08b21f0af197d6bfb06b051dd70f58b16bf9a581
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-02-11 15:01:23 -0800

    Create basic qualify page layout {VE}

---
+++ Client/Qualify/qualifyPage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Question from './questionTemplate.jsx';
+
+
+export default () => (
+  <div>
+    <Question />
+  </div>
+);
---
+++ Client/Qualify/questionTemplate.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default ({ question, response, buttonText}) => {
+  return (
+    <div>
+      <h2>Do you own your home?</h2>
+    </div>
+  );
+};
