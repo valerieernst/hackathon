b3be5246f9c3bb42ac40615f9a6ee9b3a07b6029
Iana Andrychowicz <filonenko.jana@gmail.com>
2017-02-11 15:33:35 -0800

    fix router, add jumbotron

--- Client/index.jsx
+++ Client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory, Router, Route, Link } from 'react-router';
+import { browserHistory, Router, Route } from 'react-router';
 
 import App from './app/app.jsx';
 import Investors from './Investors/Investors.jsx';
@@ -9,6 +9,7 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <Route path="investors" component={Investors} />
+      <Route path="owners" component={Investors} />
     </Route>
   </Router>,
   document.getElementById('root'),
