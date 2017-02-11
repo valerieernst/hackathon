ccfacefab8a1698b79763780158cad779a7855ac
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-02-11 12:58:26 -0800

    Serve test file in dist folder {VE}

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8008;
 
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(port, () => {
   console.log('app is listening on 8008');
