0040580ef72d7b7cb83cd01e4f97c3aae551ce89
Iana Andrychowicz <filonenko.jana@gmail.com>
2017-02-11 15:33:35 -0800

    fix router, add jumbotron

--- Client/app/app.jsx
+++ Client/app/app.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Button } from 'react-bootstrap';
+import { Button, Jumbotron } from 'react-bootstrap';
 
 import NavBar from '../nav.jsx';
 
-const App = () => (
-  <div>
-    <NavBar />
-    <Button><Link to="/owners"> Investors </Link></Button>
-    <Button> <Link to="/investors"> Investors </Link> </Button>
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Jumbotron>
+          <h3> 25% of homeowners in America struggle with their monthly mortgage payments</h3>
+          <p>
+            <Button > <Link to="/owners"> Owners </Link></Button>
+            <Button> <Link to="/investors"> Investors </Link> </Button>
+          </p>
+        </Jumbotron>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 export default App;
