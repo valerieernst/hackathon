d582f3ad457b1556129b6f8cfa08c6053374698d
Collin Tharp <catharp@gmail.com>
2017-02-12 00:37:01 -0800

    [FEAT] create addInvestment route {CT}

--- Database/handler.js
+++ Database/handler.js
@@ -11,7 +11,7 @@ module.exports = {
       res.sendStatus(500);
     })
   },
-  addUser: (req, res) => {
+  postUser: (req, res) => {
     // requires: username, password
     // optional: firstname, lastname, email
     db.query('INSERT INTO ' +
@@ -44,14 +44,16 @@ module.exports = {
       res.sendStatus(500);
     })
   },
-  addProperty: (req, res) => {
-    // requires: owner
+  postProperty: (req, res) => {
+    // requires: owner (username)
     // optional: address, city, state, country, zip, value, mortgage, term, monthly, invested
     db.query('INSERT INTO ' +
       'properties (owner, address, city, state, country, zip, value, mortgage, term, monthly, invested) ' +
-      `VALUES ((SELECT id FROM users WHERE username = \'${req.body.owner}\'), \'${req.body.address || ''}\', \'${req.body.city || ''}\', ` +
-      `\'${req.body.state || ''}\', \'${req.body.country || ''}\', ${req.body.zip || 0}, ` +
-      `${req.body.value || 0}, ${req.body.mortgage || 0}, ${req.body.term || 0}, ` +
+      `VALUES ((SELECT id FROM users WHERE username = \'${req.body.owner}\'), ` +
+      `\'${req.body.address || ''}\', \'${req.body.city || ''}\', ` +
+      `\'${req.body.state || ''}\', \'${req.body.country || ''}\', ` +
+      `${req.body.zip || 0}, ${req.body.value || 0}, ` +
+      `${req.body.mortgage || 0}, ${req.body.term || 0}, ` +
       `${req.body.monthly || 0}, ${req.body.invested || 0})`)
     .then(() => res.sendStatus(201))
     .catch(err => {
@@ -60,10 +62,24 @@ module.exports = {
     })
   },
   deleteProperty: (req, res) => {
-    // requires: username, address
-    db.query(`DELETE FROM properties WHERE owner = (SELECT id FROM users WHERE username = \'${req.body.username}\') ` +
-      `AND address = \'${req.body.address}\'`)
+    // requires: address
+    db.query(`DELETE FROM properties WHERE address = \'${req.body.address}\'`)
     .then(() => res.sendStatus(200))
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    })
+  },
+  postInvestment: (req, res) => {
+    // requires: investor (username), property (address)
+    // optional: value, return, term, monthly
+    db.query('INSERT INTO ' +
+      'investments (investor, property, value, return, term, monthly) ' +
+      `VALUES ((SELECT id FROM users WHERE username = \'${req.body.investor}\'), ` +
+      `(SELECT id FROM properties WHERE address = \'${req.body.property}\'), ` +
+      `${req.body.value || 0}, ${req.body.return || 0}, ` +
+      `${req.body.term || 0}, ${req.body.monthly || 0})`)
+    .then(() => res.sendStatus(201))
     .catch(err => {
       console.error(err);
       res.sendStatus(500);
--- Server/routes.js
+++ Server/routes.js
@@ -3,10 +3,11 @@ const db = require('../Database/handler');
 module.exports = app => {
 
   app.get('/db/user', db.getUser);
-  app.post('/db/user', db.addUser);
+  app.post('/db/user', db.postUser);
   app.delete('/db/user', db.deleteUser);
   app.get('/db/property', db.getProperties);
-  app.post('/db/property', db.addProperty);
+  app.post('/db/property', db.postProperty);
   app.delete('/db/property', db.deleteProperty);
+  app.post('/db/investment', db.postInvestment);
 
 }
